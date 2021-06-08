8e2aa8e1ae15ddf0b8f73afde1033e50921181a3
enjoyuruf <1195474031@qq.com>
2021-06-08 20:49:47 +0800

    修改页面作者信息

--- tpl/content-single.php
+++ tpl/content-single.php
@@ -30,11 +30,44 @@
 		<!--<h2 style="opacity:0;max-height:0;margin:0">Comments</h2>--><!-- 评论跳转标记 -->
 	</div><!-- .entry-content -->
 	<?php the_reward(); ?>
-	<footer class="post-footer">
-	<div class="post-lincenses"><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.zh"  target="_blank" rel="nofollow"><i class="fa fa-creative-commons" aria-hidden="true"></i> <?php _e('Attribution-NonCommercial-ShareAlike 4.0 International','sakura'); /*知识共享署名-非商业性使用-相同方式共享 4.0 国际许可协议*/?></a></div>
-	<div class="post-tags">
-		<?php if ( has_tag() ) { echo '<i class="iconfont icon-tags"></i> '; the_tags('', ' ', ' ');}?>
-	</div>
-    <?php get_template_part('layouts/sharelike'); ?>
+	<footer class="post-footer" style="height:auto;">
+
+
+		<div class="reprint" id="reprint-statement">
+			
+				<div class="reprint__author">
+					<span class="reprint-meta" style="font-weight: bold;">
+						<i class="fa fa-user" style="width:15px"></i>文章作者:
+					</span>
+					<span class="reprint-info">
+					<a href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID'))); ?>" itemprop="url" rel="author"><?php the_author(); ?></a>
+					</span>
+				</div>
+				<div class="reprint__type">
+					<span class="reprint-meta" style="font-weight: bold;">
+						<i class="fa fa-link" style="width:15px"></i>文章链接:
+					</span>
+					<span class="reprint-info">
+						<a href="<?php echo home_url(add_query_arg(array()));  ?>"><?php echo home_url(add_query_arg(array()));  ?></a>
+					</span>
+				</div>
+				<div class="reprint__notice">
+					<span class="reprint-meta" style="font-weight: bold;">
+						<i class="fa fa-copyright" style="width:15px"></i>版权声明:
+					</span>
+					<span class="reprint-info">
+						本博客所有文章除特別声明外，均采用
+						<a href="https://creativecommons.org/licenses/by/4.0/deed.zh" rel="external nofollow noreferrer" target="_blank">CC BY 4.0</a>
+						许可协议。转载请注明来源
+						<a href="https://buzhimaple.com/" target="_blank">buzhimaple</a>
+						!
+					</span>
+				</div>
+			
+		</div>
+		<div class="post-tags">
+			<?php if ( has_tag() ) { echo '<i class="iconfont icon-tags"></i> '; the_tags('', ' ', ' ');}?>
+		</div>
+		<?php get_template_part('layouts/sharelike'); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
